b22d948194db6b77dc6f081198620c8dfac6ee68
Mingyi.Li <mingyi.li@achita.com.cn>
2024-10-15 23:33:27 +0800

    complete initial version

--- README.md
+++ README.md
@@ -12,18 +12,75 @@ This tool is to kill the rubbish time after trigger long time build task.
 
 ## Install
 
-W.I.P
+- Run Powersell with Administrator permission
+
+```
+# it will download NSSM to C:\Program Files\NSSM, NSSM is a wrapper for this ps1 script
+.\caff.ps1 -Install
+```
+
+- if you have already installed NSSM, you can install by 
+
+```
+.\caff.ps1 -Install -NssmPath <YOUR_NSSM_PATH>
+
+```
+
+- if you want to remove the service (Admin is required)
+```
+.\caff.ps1 -UnInstall
+```
+
+- check the status by
+```
+.\caff.ps1 -Status
+```
+
 
 ## Usage
+### service
+- start/stop (Admin is required)
 
-W.I.P
+```
+.\caff.ps1 -Start
+
+.\caff.ps1 -Stop
+```
+### Cli
+```
+# use default config file $HOME/.caff.conf
+.\caff.ps1 
+
+# use specified config file
+.\caff.ps1 -c <YOUR_CONFIG_FILE>
+
+# scan processes every 1s, default 5s
+.\caff.ps1 -i 1
+```
+
+### Config file
+example
+```
+# process=affinity, priority"
+# get process name by, open task manager right click on column, select process Name to show"
+# affinity: 0+2 mean core0 and core2"
+# priority: idle, low, below normal, normal, above normal, high priority, realtime"
+
+explorer=0+2+4+6,realtime
+```
+
+Notice if configure file is changed, please restart service
 
 ## Other solution
 ### Power Options
-You can also change the policy on how window schedule processes, by enable hidden option in Power Options, and different option will change the behavoir on how to schedule different cores.
+You can also change the policy on how window scheduler processes, by enable hidden option in Power Options
 ```
-HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\93b8b6dc-0698-4d1c-9ee4-0644e900c85d 
-HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\7f2f5cfa-f10c-4823-b5e1-e93ae85f46b5
-...
-```
+:: enable advanced power setting
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\7f2f5cfa-f10c-4823-b5e1-e93ae85f46b5" /v Attributes /t REG_DWORD /d 2 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\93b8b6dc-0698-4d1c-9ee4-0644e900c85d" /v Attributes /t REG_DWORD /d 2 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\bae08b81-2d5e-4688-ad6a-13243356654b" /v Attributes /t REG_DWORD /d 2 /f
+
+```bat
+different option will change the behavoir on how to schedule different cores.( You can open Power Options to change the policy then )
+
 you can find more details by searching the keywords.
--- caff.ps1
+++ caff.ps1
@@ -3,6 +3,8 @@ param(
     [string]$ConfigFilePath = "$HOME\.caff.conf",
     [Alias("i")]
     [double]$Interval = 5,
+    # install patameter
+    [string]$NssmPath,
     [switch]$Install,
     [switch]$Uninstall,
     [switch]$Start,
@@ -10,9 +12,21 @@ param(
     [switch]$Status
 
 )
-
+$configTemplate = @("# process=affinity, priority",
+                    "# get process name by, open task manager right click on column, select process Name to show"
+                    "# affinity: 0+2 mean core0 and core2",
+                    "# priority: idle, low, below normal, normal, above normal, high priority, realtime",
+                    "# eg:",
+                    "# explorer=0+2+4+6,realtime")
+                    
 function Read-Config{
-    $config = Get-Content -Path $ConfigFilePath | Where-Object { $_ -notmatch '^#' } | ForEach-Object {
+
+    if ( -not (Test-Path $ConfigFilePath) ){
+        New-Item -ItemType File -Path $ConfigFilePath -Force | Out-Null
+        Add-Content -Path $ConfigFilePath -Value $configTemplate
+    }
+
+    $ret = Get-Content -Path $ConfigFilePath | Where-Object { $_ -notmatch '^#' } | ForEach-Object {
         $line = $_.Split('=')
         if ($line.Count -eq 2) {
             $keyword, $value = $line[0].Trim(), $line[1].Trim()
@@ -33,14 +47,17 @@ function Read-Config{
             }
         }
     }
-    return $config
+    return $ret
 }
+
 function Main {
     echo "start"
+    Write-Host $config
     $config = Read-Config
-    
+    Write-Host $config
+
     while ($true) {
-    
+
         foreach ($item in $config) {
             $processes = Get-Process -Name $item.Keyword -ErrorAction SilentlyContinue
     
@@ -57,14 +74,18 @@ function Main {
 #
 # for service install 
 #
-$serviceName    = 'FuckIntel'
+$serviceName    = 'CoreAffinity'
 $url            = 'https://nssm.cc/release/nssm-2.24.zip'
-$zipFile        = "$Home\Downloads\nssm-2.24.zip"
-$configFile     =  "$Home\.fuck_intel.conf"
+$zipFile        = "nssm-2.24.zip"
 $programDir     = "C:\Program Files\NSSM"
 $nssmExePath    = "$programDir\nssm.exe"
-
+$unzipDir       = "nssm-2.24"
 function get_nssm{
+    # use the value specified in arg
+    if( $NssmPath ){
+        return $NssmPath
+    }
+
     $nssm = (Get-Command nssm -ErrorAction SilentlyContinue).Source
 
     if ($nssm) {
@@ -73,20 +94,24 @@ function get_nssm{
     }
 
     $nssm = $nssmExePath
-    if (-not $nssm){
-        Write-Host "nssm not found. Downloading and extracting..."
-    
-        Invoke-WebRequest -Uri $url -OutFile $zipFile
-    
-        Remove-Item -Path "$destinationPath" -Force -Recurse
+    if (-not (Test-Path $nssm)){
+        if ( -not (Test-Path $unzipDir)) {
+            Write-Host "nssm not found. Downloading and extracting..."
+            Invoke-WebRequest -Uri $url -OutFile $zipFile
+            Expand-Archive -Path $zipFile -DestinationPath .
+        }
 
-        Expand-Archive -Path $zipFile -DestinationPath .
-    
-        New-Item -ItemType Directory -Path $programDir
+        try{
+            New-Item -ItemType Directory -Path $programDir -ErrorAction Stop | Out-Null
+            Move-Item -Path "$unzipDir\win64\nssm.exe" -Destination $nssmExePath -ErrorAction Stop | Out-Null
+        }catch{
+            # you don't have permission maybe
+            Write-Host "Fatal: can not move nssm, please run this by admin user again"
+            $nssm = ""
+        }
 
-        Move-Item -Path "nssm-2.24\win64\nssm.exe" -Destination $nssmExePath
-
-        Remove-Item -Path "$destinationPath" -Force -Recurse
+        # remove tmp file
+        # Remove-Item -Path "$unzipDir" -Force -Recurse
 
     }
     return $nssm 
@@ -95,10 +120,14 @@ function get_nssm{
 if( $Install ) {
     
     $serviceScriptPath = "$PSScriptRoot/caff.ps1"
-    $arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}" -c {1}' -f $serviceScriptPath,$configFile
+    $arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}" -c {1}' -f $serviceScriptPath,$configFilePath
     $powershell = (Get-Command powershell).Source
     $nssm=get_nssm
-    echo $nssm install $serviceName $powershell $arguments
+    if ( -not $nssm){
+        Write-Host "Fatal: failed to get nssm"
+        exit -1
+    }
+    Write-Host $nssm install $serviceName $powershell $arguments
     & $nssm install $serviceName $powershell $arguments
 
 }
